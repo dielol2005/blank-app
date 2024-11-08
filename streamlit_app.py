3ef4c71e75d615a0b0151a1025923f4d23a93362
dielol2005 <sasdiego26@gmail.com>
2024-11-08 16:01:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
 st.title("Uber pickups in NYC")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.write()
